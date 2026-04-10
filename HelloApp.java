99bf86976e4d82139cdc03f9203604ab16c5a53e
guttikondasivamadhavareddy-lgtm <guttikondasivamadhavareddy@gmail.com>
2026-04-10 14:09:17 +0530

    uc3 completed

--- HelloApp.java
+++ HelloApp.java
@@ -1,6 +1,10 @@
-public class HelloApp {
-    public static void main(String[] args) {
-        String name = args[0];   // get first argument
-        System.out.println("Hello " + name);
+public class HelloApp{
+    public static void main(String[] args){
+        String name ="world";
+        if(args.length>0)
+        {
+            name=args[0];
+        }
+        System.out.println("Hello " + name + "!");
     }
 }
